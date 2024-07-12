b7257005d3ff38bec27463e1e8f146fd42930ccd
FM <128029744+faheemmahmood@users.noreply.github.com>
2024-07-12 15:47:53 +0500

    accepting change

--- tunerhub/src/App.js
+++ tunerhub/src/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
 import NotFoundPage from './components/NotFoundPage';
 import LoginPage from './components/LoginPage';
->>>>>>> e63ca0a288a21c646dc1e5d6a64c8f939af09de2
 
 // Import your components
 import HomePage from './components/HomePage';
@@ -19,7 +14,6 @@ import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Switch>
         <Route path="/" exact component={HomePage} />
@@ -30,17 +24,6 @@ function App() {
         <Redirect to="/404" />
       </Switch>
     </Router>
-=======
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
->>>>>>> e63ca0a288a21c646dc1e5d6a64c8f939af09de2
   );
 }
 
