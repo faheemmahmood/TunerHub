3f29b61dd99e53f15e3441b6f57528b623bfc67b
FM <128029744+faheemmahmood@users.noreply.github.com>
2024-07-12 15:44:28 +0500

    removing protected route

--- tunerhub/src/App.js
+++ tunerhub/src/App.js
@@ -1,13 +1,36 @@
+<<<<<<< HEAD
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
 import NotFoundPage from './components/NotFoundPage';
 import LoginPage from './components/LoginPage';
-import ProtectedRoute from './components/ProtectedRoute';
+>>>>>>> e63ca0a288a21c646dc1e5d6a64c8f939af09de2
+
+// Import your components
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
+import NotFoundPage from './components/NotFoundPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
+<<<<<<< HEAD
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/404" component={NotFoundPage} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+=======
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -15,9 +38,9 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
-        <Route path="/protected" element={<ProtectedRoute component={ContactPage} />} />
       </Routes>
     </Router>
+>>>>>>> e63ca0a288a21c646dc1e5d6a64c8f939af09de2
   );
 }
 
